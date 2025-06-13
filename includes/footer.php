f82b8133dfa3bc191df7669b3beab35b28826ee2
fatwa08 <fatwarangkuti2@gmail.com>
2025-06-13 19:01:34 +0700

    first commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="footer-content">
         <div class="footer-info">
-            <h3>Situs Berita</h3>
+            <h3>Berita tebaru</h3>
             <p>Medan, Sumatera Utara, Indonesia</p>
-            <p>Email: fatwarangkuti8@gmail.com | Telepon: 0812-9385-0631</p>
+            <p>Email: naizulfahmi1@gmail.com | Telepon: 0812-6578-4621</p>
         </div>
-        <p class="Fatwa">&copy; <?php echo date('2025'); ?> .</p>
+        <p class="2025">&copy; <?php echo date('2025'); ?> .</p>
     </div>
 </footer>
 
